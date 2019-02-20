87bd50aab00a2486ac995cc1d74ff91d842e5135
Sakti Dewantoro <saktyd@gmail.com>
2019-02-20 16:13:15 +0700

    Improve create new user function

--- index.js
+++ index.js
@@ -8,11 +8,35 @@ const url = process.env.URL
 // Database Name
 const dbName = process.env.DB_NAME
 
+const createNewUser = function(db) {
+  const newUser = {
+    name: 'Sakti Dewantoro',
+    age: 23,
+    email: 'saktyd@gmail.com'
+  }
+
+  db.collection('users').insert(newUser, (err, result) => {
+    if (err) {
+      console.log({
+        message: 'Failed to create new user'
+      })
+    } else {
+      console.log({
+        message: 'Create new user',
+        result: result
+      })
+    }
+  })
+}
+
 // Use connect method to connect to the server
 MongoClient.connect(url, function(err, client) {
   console.log('Connected successfully to server')
 
   const db = client.db(dbName)
+
+  // insertDocument
+  createNewUser(db)
 
   client.close()
 })
