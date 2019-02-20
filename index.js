41e77d63c84cdc2db90b280142f3d1680cc5a272
Sakti Dewantoro <saktyd@gmail.com>
2019-02-20 14:24:28 +0700

    experiment with PouchDB

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hello World');
+const PouchDB = require('pouchdb');
+const db = new PouchDB('my_database');
+
+const addUser = name => {
+  const newUser = {
+    _id: '1',
+    name: name
+  };
+  db.put(newUser);
+};
+
+const getUser = id => {
+  db.get(id).then(user => {
+    console.log(user);
+  });
+};
+
+addUser('Sakti');
+getUser('1');
