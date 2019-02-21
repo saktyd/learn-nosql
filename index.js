e97f599d364749d2b0d629760b3f176d9266beb8
Sakti Dewantoro <saktyd@gmail.com>
2019-02-21 11:43:59 +0700

    Implement get hello, get list of users, create new user and delete all

--- index.js
+++ index.js
@@ -14,22 +14,50 @@ mongoose.connect(`${process.env.URL}/${process.env.DB_NAME}`, {
   useNewUrlParser: true
 })
 
-const User = mongoose.model('User', {
+const Users = mongoose.model('User', {
   name: String,
   age: Number,
   email: String
 })
 
+// Get hello
 app.get('/', (req, res) => {
   res.send({
     message: 'Hello World'
   })
 })
 
+// Get list of users
 app.get('/users', async (req, res) => {
   res.send({
     message: 'List of all user',
-    users: await User.find()
+    users: await Users.find()
+  })
+})
+
+// Create new user
+app.post('/users', async (req, res) => {
+  if (req.body.name) {
+    const newUser = new Users({
+      name: req.body.name,
+      age: req.body.age,
+      email: req.body.email,
+      birthDate: req.body.birthDate
+    })
+
+    await newUser.save()
+    res.send({
+      message: 'Created new user',
+      newUser: newUser
+    })
+  }
+})
+
+// Delete all users
+app.delete('/users', async (req, res) => {
+  res.send({
+    message: 'All users has been deleted',
+    Users: await Users.deleteMany()
   })
 })
 
